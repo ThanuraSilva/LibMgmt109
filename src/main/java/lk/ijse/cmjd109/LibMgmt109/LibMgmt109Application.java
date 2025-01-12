9d803d22288d9e1dcd5cea0afa5087f26702d47e
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-12 10:02:27 +0530

    Project init with lombok dependency optimized

---
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/LibMgmt109Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd109.LibMgmt109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMgmt109Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibMgmt109Application.class, args);
+	}
+
+}
