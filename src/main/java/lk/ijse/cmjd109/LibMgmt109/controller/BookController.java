53a62656dc86ae14d4f2934efde4dc59b65e25e4
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-12 13:41:45 +0530

    Init and test get all books operation

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
@@ -30,7 +33,7 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @GetMapping(value = "{bookId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public BookDTO getSelectedBook(@PathVariable String bookId){
+    public ResponseEntity<BookDTO> getSelectedBook(@PathVariable String bookId){
         System.out.println(bookId + " is selected");
         BookDTO book = new BookDTO();
 
@@ -44,7 +47,28 @@ public class BookController {
         book.setTotalQty(100);
         book.setAvilableQty(80);
         book.setAddedDate("2025-01-12");
-        return book;
+        return ResponseEntity.ok(book);
+    }
+    @GetMapping(value = "getall",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<BookDTO>> getAllBooks(){
+        List<BookDTO> books = new ArrayList<>();
+
+        books.add(new BookDTO("B12345", "Effective Java", "Addison-Wesley",
+                "978-0134685991", "Joshua Bloch", "3rd", 45.99, 100, 80, "2025-01-12"));
+
+        books.add(new BookDTO("B12346", "Clean Code", "Prentice Hall",
+                "978-0132350884", "Robert C. Martin", "1st", 39.99, 120, 110, "2025-01-11"));
+
+        books.add(new BookDTO("B12347", "Design Patterns", "Addison-Wesley",
+                "978-0201633610", "Erich Gamma", "1st", 55.99, 200, 190, "2025-01-10"));
+
+        books.add(new BookDTO("B12348", "Refactoring", "Addison-Wesley",
+                "978-0134757599", "Martin Fowler", "2nd", 49.99, 150, 140, "2025-01-09"));
+
+        books.add(new BookDTO("B12349", "The Pragmatic Programmer", "Addison-Wesley",
+                "978-0201616224", "Andrew Hunt", "20th Anniversary Edition", 42.99, 90, 85, "2025-01-08"));
+
+        return ResponseEntity.ok(books);
     }
 
 }
