90092666b57bb5add792a4b460eeaf98e010fa2e
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-12 14:22:16 +0530

    Relevant dependency inject for BookController

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
@@ -1,6 +1,9 @@
 package lk.ijse.cmjd109.LibMgmt109.controller;
 
 import lk.ijse.cmjd109.LibMgmt109.dto.BookDTO;
+import lk.ijse.cmjd109.LibMgmt109.service.BookService;
+import lk.ijse.cmjd109.LibMgmt109.service.impl.BookServiceIMPL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +15,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+
     @GetMapping
     public String healthTest(){
         return "Book controller is running";
     }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> addBook(@RequestBody BookDTO bookDTO){
-        System.out.println(bookDTO);
+        bookService.saveBook(bookDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
     @DeleteMapping("{bookId}")
     public ResponseEntity<Void> deleteBook(@PathVariable String bookId){
-        System.out.println(bookId);
+
+
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @PatchMapping
--- src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/BookServiceIMPL.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/BookServiceIMPL.java
@@ -2,13 +2,15 @@ package lk.ijse.cmjd109.LibMgmt109.service.impl;
 
 import lk.ijse.cmjd109.LibMgmt109.dto.BookDTO;
 import lk.ijse.cmjd109.LibMgmt109.service.BookService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class BookServiceIMPL implements BookService {
     @Override
     public void saveBook(BookDTO book) {
-
+        System.out.println("From service layer"+book);
     }
 
     @Override
