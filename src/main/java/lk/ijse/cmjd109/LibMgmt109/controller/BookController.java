e998db9734ffb1eb32ff0f1b88b29acc008377a4
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-19 09:16:34 +0530

    Clean the BookController

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/controller/BookController.java
@@ -2,15 +2,11 @@ package lk.ijse.cmjd109.LibMgmt109.controller;
 
 import lk.ijse.cmjd109.LibMgmt109.dto.BookDTO;
 import lk.ijse.cmjd109.LibMgmt109.service.BookService;
-import lk.ijse.cmjd109.LibMgmt109.service.impl.BookServiceIMPL;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -34,53 +30,20 @@ public class BookController {
     }
     @DeleteMapping("{bookId}")
     public ResponseEntity<Void> deleteBook(@PathVariable String bookId){
-
-
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @PatchMapping
     public ResponseEntity<Void> updateBook(@RequestParam ("bookId") String id, @RequestBody BookDTO bookDTO){
-        System.out.println(id);
-        System.out.println(bookDTO);
+        bookService.updateBook(id, bookDTO);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @GetMapping(value = "{bookId}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BookDTO> getSelectedBook(@PathVariable String bookId){
-        System.out.println(bookId + " is selected");
-        BookDTO book = new BookDTO();
-
-        book.setBookId("B12345");
-        book.setTitle("Effective Java");
-        book.setPublisher("Addison-Wesley");
-        book.setIsbn("978-0134685991");
-        book.setAuthor("Joshua Bloch");
-        book.setEdition("3rd");
-        book.setPrice(45.99);
-        book.setTotalQty(100);
-        book.setAvilableQty(80);
-        book.setAddedDate("2025-01-12");
-        return ResponseEntity.ok(book);
+        return ResponseEntity.ok(bookService.getSelectedBook(bookId));
     }
     @GetMapping(value = "getall",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<BookDTO>> getAllBooks(){
-        List<BookDTO> books = new ArrayList<>();
-
-        books.add(new BookDTO("B12345", "Effective Java", "Addison-Wesley",
-                "978-0134685991", "Joshua Bloch", "3rd", 45.99, 100, 80, "2025-01-12"));
-
-        books.add(new BookDTO("B12346", "Clean Code", "Prentice Hall",
-                "978-0132350884", "Robert C. Martin", "1st", 39.99, 120, 110, "2025-01-11"));
-
-        books.add(new BookDTO("B12347", "Design Patterns", "Addison-Wesley",
-                "978-0201633610", "Erich Gamma", "1st", 55.99, 200, 190, "2025-01-10"));
-
-        books.add(new BookDTO("B12348", "Refactoring", "Addison-Wesley",
-                "978-0134757599", "Martin Fowler", "2nd", 49.99, 150, 140, "2025-01-09"));
-
-        books.add(new BookDTO("B12349", "The Pragmatic Programmer", "Addison-Wesley",
-                "978-0201616224", "Andrew Hunt", "20th Anniversary Edition", 42.99, 90, 85, "2025-01-08"));
-
-        return ResponseEntity.ok(books);
+        return ResponseEntity.ok(bookService.getAllBooks());
     }
 
 }
--- src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/BookServiceIMPL.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/BookServiceIMPL.java
@@ -5,6 +5,7 @@ import lk.ijse.cmjd109.LibMgmt109.service.BookService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class BookServiceIMPL implements BookService {
@@ -25,11 +26,39 @@ public class BookServiceIMPL implements BookService {
 
     @Override
     public BookDTO getSelectedBook(String bookId) {
-        return null;
+        System.out.println(bookId + " is selected");
+        BookDTO book = new BookDTO();
+        book.setBookId("B12345");
+        book.setTitle("Effective Java");
+        book.setPublisher("Addison-Wesley");
+        book.setIsbn("978-0134685991");
+        book.setAuthor("Joshua Bloch");
+        book.setEdition("3rd");
+        book.setPrice(45.99);
+        book.setTotalQty(100);
+        book.setAvilableQty(80);
+        book.setAddedDate("2025-01-12");
+        return book;
     }
 
     @Override
     public List<BookDTO> getAllBooks() {
-        return List.of();
+        List<BookDTO> books = new ArrayList<>();
+
+        books.add(new BookDTO("B12345", "Effective Java", "Addison-Wesley",
+                "978-0134685991", "Joshua Bloch", "3rd", 45.99, 100, 80, "2025-01-12"));
+
+        books.add(new BookDTO("B12346", "Clean Code", "Prentice Hall",
+                "978-0132350884", "Robert C. Martin", "1st", 39.99, 120, 110, "2025-01-11"));
+
+        books.add(new BookDTO("B12347", "Design Patterns", "Addison-Wesley",
+                "978-0201633610", "Erich Gamma", "1st", 55.99, 200, 190, "2025-01-10"));
+
+        books.add(new BookDTO("B12348", "Refactoring", "Addison-Wesley",
+                "978-0134757599", "Martin Fowler", "2nd", 49.99, 150, 140, "2025-01-09"));
+
+        books.add(new BookDTO("B12349", "The Pragmatic Programmer", "Addison-Wesley",
+                "978-0201616224", "Andrew Hunt", "20th Anniversary Edition", 42.99, 90, 85, "2025-01-08"));
+        return books;
     }
 }
