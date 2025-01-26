9c22dd2579b6e8729049c2fa90ae935be5a653de
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-26 13:50:49 +0530

    Create StaffDao and add relevant mappings to EntityDTOConversion

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/dao/StaffDao.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/dao/StaffDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.cmjd109.LibMgmt109.dao;
 
-public interface StaffDao {
+import lk.ijse.cmjd109.LibMgmt109.entities.StaffEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffDao extends JpaRepository<StaffEntity,String> {
 }
--- src/main/java/lk/ijse/cmjd109/LibMgmt109/util/EntityDTOConversion.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/util/EntityDTOConversion.java
@@ -1,7 +1,10 @@
 package lk.ijse.cmjd109.LibMgmt109.util;
 
+import lk.ijse.cmjd109.LibMgmt109.dao.StaffDao;
 import lk.ijse.cmjd109.LibMgmt109.dto.BookDTO;
+import lk.ijse.cmjd109.LibMgmt109.dto.LibStaffDTO;
 import lk.ijse.cmjd109.LibMgmt109.entities.BookEntity;
+import lk.ijse.cmjd109.LibMgmt109.entities.StaffEntity;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -23,6 +26,17 @@ public class EntityDTOConversion {
     }
     public List<BookDTO> toBookDTOList(List<BookEntity> books){
         return modelMapper.map(books,new TypeToken<List<BookDTO>>(){}.getType());
+    }
+
+    // Staff
+    public LibStaffDTO toStaffDTO(StaffEntity staff) {
+        return modelMapper.map(staff, LibStaffDTO.class);
+    }
+    public StaffEntity toStaffEntity(LibStaffDTO staffDTO) {
+        return modelMapper.map(staffDTO,StaffEntity.class);
+    }
+    public List<LibStaffDTO> toStaffDTOList(List<StaffEntity> staffEntities){
+        return modelMapper.map(staffEntities,new TypeToken<List<LibStaffDTO>>(){}.getType());
     }
 
 
