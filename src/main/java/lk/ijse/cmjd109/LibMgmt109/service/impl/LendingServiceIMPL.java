8367a2616e53f588ff6d05bea9d3d3af490b997b
ThanuraSilva <thanurasilva1989@gmail.com>
2025-02-02 10:48:16 +0530

    Test the Book availability and member validation in the LendingServiceIMPL

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/LendingServiceIMPL.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/service/impl/LendingServiceIMPL.java
@@ -1,16 +1,39 @@
 package lk.ijse.cmjd109.LibMgmt109.service.impl;
 
+import lk.ijse.cmjd109.LibMgmt109.dao.BookDao;
+import lk.ijse.cmjd109.LibMgmt109.dao.MemberDao;
 import lk.ijse.cmjd109.LibMgmt109.dto.LendingDTO;
+import lk.ijse.cmjd109.LibMgmt109.exception.BookNotFoundException;
+import lk.ijse.cmjd109.LibMgmt109.exception.MemberNotFoundException;
 import lk.ijse.cmjd109.LibMgmt109.service.LendingService;
+import lk.ijse.cmjd109.LibMgmt109.util.LendingMapping;
 import lk.ijse.cmjd109.LibMgmt109.util.UtilityData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class LendingServiceIMPL implements LendingService {
+    private final LendingMapping lendingMapping;
+    private final BookDao bookDao;
+    private final MemberDao memberDao;
+
+
     @Override
     public void addLending(LendingDTO lendingDTO) {
-       lendingDTO.setLendingId(UtilityData.generateLendingId());
+
+        String book = lendingDTO.getBook();
+        String member = lendingDTO.getMember();
+        //Availability of the book
+        bookDao.findById(book).orElseThrow(()->
+                new BookNotFoundException("Book not found"));
+        // Membership validation
+        memberDao.findById(member).orElseThrow(()->
+                new MemberNotFoundException("Member not found"));
+
     }
 
     @Override
