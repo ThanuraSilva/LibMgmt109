3891ca5c6715786556351de71f9a8660172cc05e
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-16 13:55:49 +0530

    Implemented WebSecurityConfig

--- src/main/java/lk/ijse/cmjd109/LibMgmt109/secureConfig/CORSConfig.java
+++ src/main/java/lk/ijse/cmjd109/LibMgmt109/secureConfig/CORSConfig.java
@@ -1,11 +1,9 @@
 package lk.ijse.cmjd109.LibMgmt109.secureConfig;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
